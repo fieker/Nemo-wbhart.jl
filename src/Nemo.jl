594f8375f0ce1606405171dbfa13f234f3bb0b25
Tommy Hofmann <thofma@gmail.com>
2016-03-17 15:04:46 +0100

    Fix Win64 build and add appveyor support

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -84,12 +84,14 @@ function __init__()
       push!(Libdl.DL_LOAD_PATH, libdir)
    end
  
-   ccall((:pari_set_memory_functions, libpari), Void,
-      (Ptr{Void},Ptr{Void},Ptr{Void},Ptr{Void}),
-      cglobal(:jl_malloc),
-      cglobal(:jl_calloc),
-      cglobal(:jl_realloc),
-      cglobal(:jl_free))
+   @linux ? begin
+      ccall((:pari_set_memory_functions, libpari), Void,
+         (Ptr{Void},Ptr{Void},Ptr{Void},Ptr{Void}),
+         cglobal(:jl_malloc),
+         cglobal(:jl_calloc),
+         cglobal(:jl_realloc),
+         cglobal(:jl_free))
+   end : nothing
 
    ccall((:pari_init, libpari), Void, (Int, Int), 300000000, 10000)
   
@@ -103,18 +105,20 @@ function __init__()
 
    unsafe_store!(pari_sigint, cfunction(pari_sigint_handler, Void, ()), 1)
 
-   ccall((:__gmp_set_memory_functions, libgmp), Void,
-      (Ptr{Void},Ptr{Void},Ptr{Void}),
-      cglobal(:jl_gc_counted_malloc),
-      cglobal(:jl_gc_counted_realloc_with_old_size),
-      cglobal(:jl_gc_counted_free))
+   @linux ? begin
+      ccall((:__gmp_set_memory_functions, libgmp), Void,
+         (Ptr{Void},Ptr{Void},Ptr{Void}),
+         cglobal(:jl_gc_counted_malloc),
+         cglobal(:jl_gc_counted_realloc_with_old_size),
+         cglobal(:jl_gc_counted_free))
 
-   ccall((:__flint_set_memory_functions, libflint), Void,
-      (Ptr{Void},Ptr{Void},Ptr{Void},Ptr{Void}),
-      cglobal(:jl_malloc),
-      cglobal(:jl_calloc),
-      cglobal(:jl_realloc),
-      cglobal(:jl_free))
+      ccall((:__flint_set_memory_functions, libflint), Void,
+         (Ptr{Void},Ptr{Void},Ptr{Void},Ptr{Void}),
+         cglobal(:jl_malloc),
+         cglobal(:jl_calloc),
+         cglobal(:jl_realloc),
+         cglobal(:jl_free))
+   end : nothing
 
    println("")
    println("Welcome to Nemo version 0.4.0")
